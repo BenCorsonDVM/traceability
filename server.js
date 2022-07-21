349250230a1e6d4979cd7136d5b3afd2d076a183
Ben Corson <corsonbenjamin@gmail.com>
2022-07-21 15:28:34 -0500

    test

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const path = require('path')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+
+
+
 
 const port = process.env.PORT || 5050
 
