ba0a22720f3295e35b194de24c7c5f3dbfa678d7
Ben Corson <corsonbenjamin@gmail.com>
2022-07-21 17:10:08 -0500

    linked rollbar

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'ede99e54fcf34cb2a0947a56e2c89371',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
@@ -19,8 +30,8 @@ app.get('/testroute', (req, res) => {
 })
 
 app.get('/press-me-button', (req, res) => {
-    rollbar.info('Someone pressed the button')
     res.send('Autosave is OP')
+    rollbar.info('Someone pressed the button')
 })
 
 
